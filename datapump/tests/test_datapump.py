9fba0c53212346695ef858fbbdc7ab93d6463ea5
daquinoaldo <daquino.aldo@gmail.com>
2018-11-16 00:29:45 +0100

    Configure Travis, pytest and coverage

---
+++ datapump/tests/test_datapump.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestDatapump(unittest.TestCase):
+
+    # Fake test, is needed to prepare the travis configuration. Must be filled
+    def test_one(self):
+        self.assertEqual(True, True)
